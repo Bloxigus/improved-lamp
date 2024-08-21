ba578efba843d289772c0767f24dfaa5834a63b4
GitHub Action <action@github.com>
2024-08-21 06:27:22 +0000

    Initialize template repository proper
    Set package name to com.github.bloxigus.improvedlamp
    Set project name to improved-lamp
    Set modid to improvedlamp

--- src/main/java/com/github/bloxigus/improvedlamp/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/bloxigus/improvedlamp/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.bloxigus.improvedlamp.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
